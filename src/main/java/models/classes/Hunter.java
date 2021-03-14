f7b598605e6edd40228273f32cdc97b85ff91cb5
Matthew <m.mccoach88@gmail.com>
2021-03-14 13:29:28 +0000

    Created hunter subclass extending character

--- src/main/java/models/classes/Hunter.java
+++ src/main/java/models/classes/Hunter.java
@@ -1,6 +1,22 @@
 package models.classes;
 import models.Character;
+import models.RaceType;
+import models.specialisations.HunterSpec;
 
 public class Hunter extends Character {
 
+    private HunterSpec specialisation;
+
+    public Hunter(String name, int level, RaceType race, HunterSpec specialisation) {
+        super(name, level, race);
+        this.specialisation = specialisation;
+    }
+
+    public HunterSpec getSpecialisation() {
+        return specialisation;
+    }
+
+    public void setSpecialisation(HunterSpec specialisation) {
+        this.specialisation = specialisation;
+    }
 }
---
+++ src/main/java/models/specialisations/HunterSpec.java
@@ -0,0 +1,7 @@
+package models.specialisations;
+
+public enum HunterSpec {
+    MARKSMANSHIP,
+    SURVIVAL,
+    BEASTMASTERY
+}
