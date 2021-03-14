cf69f4ec6313c9ae859cdc3a5608c7e88bd6763d
Matthew <m.mccoach88@gmail.com>
2021-03-14 13:25:52 +0000

    Refactored character superclass

--- src/main/java/models/Character.java
+++ src/main/java/models/Character.java
@@ -2,10 +2,37 @@ package models;
 
 public abstract class Character {
 
+    private String name;
+    private int level;
+    private RaceType race;
+
     public Character(String name, int level, RaceType race) {
-        name = name;
-        level = level;
-        race = race;
+        this.name = name;
+        this.level = level;
+        this.race = race;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public RaceType getRace() {
+        return race;
+    }
+
+    public void setRace(RaceType race) {
+        this.race = race;
+    }
 }
---
+++ src/main/java/models/classes/Hunter.java
@@ -0,0 +1,6 @@
+package models.classes;
+import models.Character;
+
+public class Hunter extends Character {
+
+}
